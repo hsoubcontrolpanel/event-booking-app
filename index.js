d551b4dd02039ffe2de9b40e6d86e8e90564a101
hsoubcontrolpanel <hsoubcontrolpanel@gmail.com>
2022-03-08 16:45:04 +0200

    DB Done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const http = require('http')
 const { typeDefs } = require('./schema/index')
 const { resolvers } = require('./resolvers/index')
 require('dotenv').config()
+const mongoose = require('mongoose')
 
 async function startApolloServer(typeDefs, resolvers){
     const app = express()
@@ -18,5 +19,12 @@ async function startApolloServer(typeDefs, resolvers){
     server.applyMiddleware({ app })
     await new Promise(resolve => httpServer.listen({ port: process.env.PORT }, resolve))
     console.log(`Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`)
+    mongoose.connect(process.env.DB_URL,
+        err => {
+            if(err) throw err
+            console.log('DB connected succssfully')
+        }
+    )
+    return { server, app }
 }
 startApolloServer(typeDefs, resolvers)
